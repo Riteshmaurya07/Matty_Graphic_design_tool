76a4c974cff41f11fccbbe127e44599e41713727
Ritesh Maurya <riteshmauryarm563@gmail.com>
2025-08-19 12:36:46 +0530

    frontend url is stored in env file

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    // origin: "http://localhost:5173",
+    origin: process.env.frontend_url || "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
